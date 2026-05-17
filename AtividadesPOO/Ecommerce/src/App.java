4d91ffeb5e97ad9572089a1581b4f8dc0925f689
Marckinglording <dev.marcoslima@gmail.com>
2026-05-17 13:16:07 -0300

    Estrutura de App iniciada

--- AtividadesPOO/Ecommerce/src/App.java
+++ AtividadesPOO/Ecommerce/src/App.java
@@ -3,11 +3,28 @@ import java.util.Scanner;
 
 public class App {
 
-    Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
 
-        
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\nCADASTRO DE PRODUTOS\n");
+
+        System.out.println("Escolha o tipo de produto: ")
+
+        System.out.println("1- Produto Físico");
+        System.out.println("2- Produto Digital");
+        System.out.println("3- Assinatura");
+
+        System.out.println("Nome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Preço: ");
+        double preco = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("Peso: ");
+        double peso = scan.nextDouble();
+        scan.nextLine();
+
     }
 }
