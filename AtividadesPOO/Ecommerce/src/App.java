4618970e3987c179a462c9ec42bb199b4f71482a
Marckinglording <dev.marcoslima@gmail.com>
2026-05-17 12:48:53 -0300

    Carrinho pronto e início da criação do App

--- AtividadesPOO/Ecommerce/src/App.java
+++ AtividadesPOO/Ecommerce/src/App.java
@@ -1,7 +1,13 @@
 package AtividadesPOO.Ecommerce.src;
+import java.util.Scanner;
 
 public class App {
+
+    Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
+
+        
         
     }
 }
--- AtividadesPOO/Ecommerce/src/Carrinho.java
+++ AtividadesPOO/Ecommerce/src/Carrinho.java
@@ -3,15 +3,21 @@ import java.util.ArrayList;
 
 public class Carrinho {
 
-    ArrayList<Produto> produtos = new ArrayList<>();
+    ArrayList<Produto> produtos = new ArrayList<>();  
 
-    public void calcularTotal() {
+    public void adicionarProduto(Produto produtoAdicionado){
+        produtos.add(produtoAdicionado);
+    }
+
+    public double calcularTotal() {
 
         double total= 0;
 
         for(int i = 0; i < produtos.size(); i++) {
             total += produtos.get(i).calcularPrecoFinal();
         }
+
+        return total;
 
     }
     
