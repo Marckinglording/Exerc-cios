d11d43ec1ab6470f41a4f5d2629cf0b6b072a8d0
Marckinglording <dev.marcoslima@gmail.com>
2026-05-17 13:22:38 -0300

    Atualização em App

--- AtividadesPOO/Ecommerce/src/App.java
+++ AtividadesPOO/Ecommerce/src/App.java
@@ -9,11 +9,20 @@ public class App {
 
         System.out.println("\nCADASTRO DE PRODUTOS\n");
 
-        System.out.println("Escolha o tipo de produto: ")
+        System.out.println("Escolha o tipo de produto: ");
 
         System.out.println("1- Produto Físico");
         System.out.println("2- Produto Digital");
         System.out.println("3- Assinatura");
+
+        int tipoProduto = scan.nextInt();
+        scan.nextLine();
+
+        switch (tipoProduto) {
+
+            case 1:
+                
+        }
 
         System.out.println("Nome: ");
         String nome = scan.nextLine();
