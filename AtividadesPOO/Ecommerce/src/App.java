ad4fcce455545656515d3cf6ecd53cc3bdaf40bb
Marckinglording <dev.marcoslima@gmail.com>
2026-05-17 22:59:19 -0300

    Projeto 1 funcionando, ainda cabendo alterações e melhorias

--- AtividadesPOO/Ecommerce/src/App.java
+++ AtividadesPOO/Ecommerce/src/App.java
@@ -6,34 +6,107 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
+        Carrinho carrinhoDeCompras = new Carrinho();
+        int tipoProduto = 0;
 
-        System.out.println("\nCADASTRO DE PRODUTOS\n");
+        System.out.println("\nCADASTRO DE PRODUTOS");
 
-        System.out.println("Escolha o tipo de produto: ");
+        while(true) {
 
-        System.out.println("1- Produto Físico");
-        System.out.println("2- Produto Digital");
-        System.out.println("3- Assinatura");
+            while (true) {
+            
+                System.out.println("\nEscolha o tipo de produto: \n");
 
-        int tipoProduto = scan.nextInt();
-        scan.nextLine();
+                System.out.println("1- Produto Físico");
+                System.out.println("2- Produto Digital");
+                System.out.println("3- Assinatura\n");
 
-        switch (tipoProduto) {
+                if(scan.hasNextInt()) {
 
-            case 1:
+                    int escolha = scan.nextInt();
+
+                    if(escolha > 0 && escolha < 4) {
+                        tipoProduto = escolha;
+                        scan.nextLine();
+                        break;
+                    }else {
+                        System.out.println("Entrada inválida! \nDigite um número correspondente à uma das opções.");
+                        continue;
+                    }
+
+                }else {
+                    System.out.println("Entrada inválida! \nDigite apenas números.");
+                    scan.nextLine();
+                }
+
+            }
                 
+            System.out.println("\nNome:");
+            String nome = scan.nextLine();
+
+            System.out.println("\nPreço:");
+            double preco = scan.nextDouble();
+            scan.nextLine();
+
+            System.out.println("\nPeso:");
+            double peso = scan.nextDouble();
+            scan.nextLine();
+
+            Produto novoProduto = null;
+
+            switch (tipoProduto) {
+
+                case 1:
+                    novoProduto = new ProdutoFisico(nome, preco, peso);
+                    break;
+
+                case 2:
+                    novoProduto = new ProdutoDigital(nome, preco, peso);
+                    break;
+                    
+                case 3:
+                    novoProduto = new Assinatura(nome, preco, peso);
+                    break;
+
+                default:
+                    System.out.println("Erro ao adicionar produto");
+                    break;
+
+                }
+
+            carrinhoDeCompras.adicionarProduto(novoProduto);
+            System.out.println("\nO total do seu carrinho é de: " + carrinhoDeCompras.calcularTotal());
+
+            System.out.println("\nDeseja adicionar outro produto?");
+            System.out.println("1- Sim");
+            System.out.println("2- Não");
+
+            if(scan.hasNextInt()) {
+
+                int escolha = scan.nextInt();
+
+                if(escolha > 0 && escolha < 3) {
+                        
+                    scan.nextLine();
+
+                    if (escolha == 1) {
+                        continue;
+                    }
+                    if (escolha == 2) {
+                        break;
+                    }
+
+                }else {
+                    System.out.println("Entrada inválida! \nDigite um número correspondente à uma das opções.");
+                    continue;
+                }
+
+            }else {
+                System.out.println("Entrada inválida! \nDigite apenas números.");
+                scan.nextLine();
+            }
+
         }
-
-        System.out.println("Nome: ");
-        String nome = scan.nextLine();
-
-        System.out.println("Preço: ");
-        double preco = scan.nextDouble();
-        scan.nextLine();
-
-        System.out.println("Peso: ");
-        double peso = scan.nextDouble();
-        scan.nextLine();
 
     }
 }
